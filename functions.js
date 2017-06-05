5f18cfc314f4849b29d12aafe08b791f35d1cc1f
Jesse Copeland <jesse.copeland@gmail.com>
2017-06-05 10:00:52 -1000

    complete

--- functions.js
+++ functions.js
@@ -125,7 +125,9 @@ function calculate(op, x, y) {
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
-
+function isGreaterThan(a, b) {
+  return a > b;
+}
 
 /**
  * Returns true if `a` is less than `b`.
@@ -133,7 +135,9 @@ function calculate(op, x, y) {
  * @param {number} b
  * @return {boolean} `a` is smaller than `b`
  */
-
+function isLessThan(a, b) {
+  return a < b;
+}
 
 /**
  * Returns true if `a` and `b` are equal.
@@ -141,6 +145,9 @@ function calculate(op, x, y) {
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
+function areEqual(a, b) {
+  return a === b;
+}
 
 
 /**
@@ -149,7 +156,13 @@ function calculate(op, x, y) {
  * @param {number} y
  * @return {number} the smallest number
  */
-
+function minimum(a, b) {
+  if (a <= b) {
+    return a;
+  } else {
+    return b;
+  }
+}
 
 /**
  * Returns the largest value of two numbers.
@@ -157,21 +170,31 @@ function calculate(op, x, y) {
  * @param {number} y
  * @return {number} the largest number
  */
-
+function maximum(a, b) {
+  if (a >= b) {
+    return a;
+  } else {
+    return b;
+  }
+}
 
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
-
+function isEven(a) {
+  return a % 2 === 0;
+}
 
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
-
+function isOdd(a) {
+  return a % 2 !== 0;
+}
 
 /**
  * Returns a letter grade.
@@ -184,6 +207,45 @@ function calculate(op, x, y) {
  * @param {number} total maximum possible score
  * @return {string} the score represented as a letter grade
  */
+function letterGrade(score, total) {
+  var percent = score / total;
+
+  console.log('percent ' + percent);
+
+  // switch (true) {
+  //   case (percent >= 0.9 && percent <= 1):
+  //     return 'A';
+  //     break;
+  //   case (percent >= 0.8 && percent < 0.9):
+  //     return 'B';
+  //     break;
+  //   case (percent >= 0.7 && percent < 0.8):
+  //     return 'C';
+  //     break;
+  //   case (percent >= 0.6 && percent < 0.7):
+  //     return 'D';
+  //     break;
+  //   case (percent >= 0 && percent < 0.6):
+  //     return 'F';
+  //     break;
+  //   default:
+  //     return percent + ' is not valid';
+  // }
+
+  if (percent >= 0.9 && percent <= 1) {
+    return 'A';
+  } else if (percent >= 0.8 && percent <= 0.9) {
+    return 'B';
+  } else if (percent >= 0.7 && percent <= 0.8) {
+    return 'C';
+  } else if (percent >= 0.6 && percent <= 0.7) {
+    return 'D';
+  } else if (percent >= 0 && percent <= 0.6) {
+    return 'F';
+  } else {
+    return percent + ' is not valid';
+  }
+}
 
 
 /**
@@ -193,7 +255,14 @@ function calculate(op, x, y) {
  * @param {object} restaurant   represents a restaurant object
  * @return {object} restaurant
  */
-
+function incrementReviews(rest) {
+  if (rest.reviews) {
+    rest.reviews++;
+  } else {
+    rest.reviews = 1;
+  }
+  return rest;
+}
 
 /**
  * Joins two strings with a space.
@@ -201,7 +270,10 @@ function calculate(op, x, y) {
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
-
+function combine(firstWord, secondWord) {
+  // return firstWord + ' ' + secondWord;
+  return `${firstWord} ${secondWord}`;
+}
 
 /**
  * Returns a circle object with the properties `circumference` and `area`.
@@ -210,4 +282,9 @@ function calculate(op, x, y) {
  * @param {number} radius
  * @return {object} circle
  */
-
+function createCircle(radius) {
+  var circle = {};
+  circle.circumference = 2 * Math.PI * radius;
+  circle.area = Math.PI * (radius ** 2);
+  return circle;
+}
